1730afe1a8f3bbbbbcc65bce24da76993866aed0
Jacob Heider <jacob@tea.xyz>
2022-05-19 10:46:36 -0400

    well, let's try it
    Signed-off-by: Jacob Heider <jacob@tea.xyz>

---
+++ bin/app.ts
@@ -0,0 +1,15 @@
+import * as cdk from '@aws-cdk/core';
+
+import TeaXYZ from '../lib/tea.xyz';
+
+const app = new cdk.App();
+
+/**
+ * The name of the stack depends on the STAGE environment variable so we can deploy the infrastructure multiple times in parallel
+ * @example
+ * AwesomeStack-pr-1-awesome-branch
+ * AwesomeStack-production
+ */
+const stackName = 'TeaXYZ-' + process.env.STAGE;
+
+new TeaXYZ(app, stackName);
